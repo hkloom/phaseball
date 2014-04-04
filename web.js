21381bf16e8f150ff18de260f6507250cfa2a3ed
Tai Rodrig <taicapra@gmail.com>
2014-04-04 09:55:21 -0700

    starting calendar app

--- web.js
+++ web.js
@@ -6,10 +6,12 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('<<<<<<<CALENDAR CREATOR>>>>>>>');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
