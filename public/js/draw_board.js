5deeff96ec35a59582dc1abaa504dfbe5858b427
Tai Rodrig <taicapra@gmail.com>
2014-05-01 18:21:11 -0700

    migrating to kineticjs

--- public/js/draw_board.js
+++ public/js/draw_board.js
@@ -37,30 +37,60 @@ function inRect(ball,rect){
 
 //---DRAWING FUNCTIONS---
 
-function drawBackground(id, board){
+function drawBackground(layer, board){
 	//---DRAW BOARD BACKGROUND---
-	canvas = document.getElementById(id);
-	ctx = canvas.getContext('2d');
-	ctx.beginPath();
-	ctx.fillStyle = "#FFFFFF";
-	ctx.rect(frame.x,frame.y,frame.width,frame.height);
-	ctx.fill();
 
+	drawRect(layer,{x: layer.x(),
+					y: layer.y(),
+					width:layer.width(),
+					height:layer.height()
+					},
+					board, "#FFFFFF");
+
+	
 	//draw vertical lines and labels
-	var xinterval = frame.width / board.width;
+	var xinterval = layer.width() / board.width;
 	for (var l=0; l<board.width; l++){
 		if (l>0){
-			ctx.beginPath();
-			ctx.strokeStyle = "#666666";
-			ctx.moveTo(l*xinterval,frame.x);
-			ctx.lineTo(l*xinterval,frame.x+frame.height);
-			ctx.stroke();
+			var line = {A: {x: l*xinterval, y:layer.x()},
+						B: {x: l*xinterval, y:layer.x()+layer.height()}};
+			drawLine(layer,line,"#DDDDDD");
 		}
+
+		var text = new Kinetic.Text({
+		  x: l*xinterval+2,
+		  y: layer.height()-15,
+		  text:  "("+ +(l+board.x)+","+ (board.y)+")",
+		  fontSize: 12,
+		  fontFamily: 'Calibri',
+		  fill: 'black'
+		});
+		layer.add(text);
+		/*
 		ctx.fillStyle = "black"
 		ctx.font="16px Georgia";
 		var fmtstr = "("+ +(l+board.x)+","+ (board.y)+")";
-		ctx.fillText(fmtstr,l*xinterval+2,frame.height-5);
+		ctx.fillText(fmtstr,l*xinterval+2,frame.height-5);*/
 	}
+
+	//draw horizontal lines and labels
+	var yinterval = layer.height() / board.height;
+	for (var l=1; l<board.height; l++){
+		var line = {A: {x: layer.x(), y:l*yinterval},
+					B: {x: layer.x()+layer.width(), y:l*yinterval}};
+		drawLine(layer,line,"#DDDDDD");
+		var text = new Kinetic.Text({
+		  x: 2,
+		  y: l*yinterval-15,
+		  text:  "("+ board.x +","+ +(board.height-l+board.y) +")",
+		  fontSize: 12,
+		  fontFamily: 'Calibri',
+		  fill: 'black'
+		});
+		layer.add(text);
+
+	}
+	/*
 	//draw horizontal lines and labels
 	var yinterval = frame.height / board.height;
 	for (var l=1; l<board.height; l++){
@@ -74,53 +104,66 @@ function drawBackground(id, board){
 		var fmtstr = "("+ board.x +","+ +(board.height-l+board.y) +")";
 		ctx.fillText(fmtstr,2,l*yinterval-5);
 	}
+	*/
 }
 
-function drawBall(id,ball,board){
-	canvas = document.getElementById(id);
-	ctx = canvas.getContext('2d');
-	ctx.fillStyle = "#FF0000";
-	ctx.beginPath();
-   	ctx.arc((ball.x-board.x)/board.width*frame.width, 
-   		frame.height-(ball.y-board.y)/board.height*frame.height,
-   		5, 0, 2 * Math.PI, false);
-    ctx.fill();
+function drawLine(layer,line,color){
+	var line = new Kinetic.Line({
+		        points: [line.A.x, line.A.y, line.B.x, line.B.y],
+		        stroke: color,
+		        strokeWidth: 2,
+		      });
+	layer.add(line);
+}
+
+function drawBall(layer,ball,board,color){
+	//console.log("layer width: "+ layer.width() );
+	var circ = new Kinetic.Circle({
+		x: (ball.x-board.x)/board.width*layer.width(),
+		y: layer.height()*(1-((ball.y-board.y)/board.height)),
+		radius: 5,
+		fill: color,
+		stroke: 'black',
+		strokeWidth: 0
+	});
+	// add the shape to the layer
+	layer.add(circ);
 }
 
 
-function drawBalls(id,balls,board,color){
+function drawBalls(layer,balls,board,color){
 	for (var i=0; i<balls.length;i++){
-		console.log(balls[i]);
+		//console.log(balls[i]);
 		//alert(JSON.stringify(obstacles[i], null, 4));
-		drawBall(id,balls[i],board);
+		drawBall(layer,balls[i],board,color);
 	}
 }
 
-function drawRect(id,goal,board, color){
-	canvas = document.getElementById(id);
-	ctx = canvas.getContext('2d');
-	ctx.fillStyle = color;
-	ctx.strokeStyle = '#000000';
-	ctx.beginPath();
-   	ctx.rect((goal.x-board.x)/board.width*frame.width, 
-   		frame.height-(goal.y-board.y)/board.height*frame.height,
-   		goal.width/board.width*frame.width,
-   		-goal.height/board.height*frame.height
-   		);
-    ctx.fill();
-    ctx.stroke();
+function drawRect(layer,goal,board,color){
+	//console.log("layer width: "+ layer.width() );
+	var rect = new Kinetic.Rect({
+		x: (goal.x-board.x)/board.width*layer.width(),
+		y: layer.height()*(1-((goal.y-board.y)/board.height)),
+		width: goal.width/board.width*layer.width(),
+		height: -goal.height/board.height*layer.height(),
+		fill: color,
+		stroke: 'black',
+		strokeWidth: 2
+	});
+	// add the shape to the layer
+	layer.add(rect);
+	//console.log(rect);
 }
 
-function drawObstacles(id,obstacles,board,color){
+
+function drawObstacles(layer,obstacles,board,color){
 	for (var i=0; i<obstacles.length;i++){
 		//alert(JSON.stringify(obstacles[i], null, 4));
-		drawRect(id,obstacles[i],board,color);
+		drawRect(layer,obstacles[i],board,color);
 	}
 }
 
-function drawBallsPath(id,balls,goal,obstacles,board,equation){
-	canvas = document.getElementById(id);
-	ctx = canvas.getContext('2d');
+function drawBallsPath(layer,balls,goal,obstacles,board,equation){
 	var step = 0.01;
 	for (var k=0;k<balls.length;k++){
 		( function() {
@@ -129,7 +172,7 @@ function drawBallsPath(id,balls,goal,obstacles,board,equation){
 			while(inRect(b,board) && !dead){
 				b.x += step*equation.dx(b.x,b.y);
 				b.y += step*equation.dy(b.x,b.y);
-				drawBall(id,b,board);
+				drawBall(layer,b,board);
 				if (inRect(b,goal)){
 					console.log("ball "+i+" hit goal!!");
 					break;
